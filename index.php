fb2aa20f1783cf3173dd636e5b0b7ce801453aeb
Anjing Militan <wijay11ipa@gmail.com>
2019-07-01 21:56:54 +0700

    form

--- index.php
+++ index.php
@@ -26,15 +26,34 @@ $conn = sqlsrv_connect($serverName, $connectionInfo);
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 <body>
-    <div class="card">
-      <div class="card-header">
-        Data Diri
-      </div>
-      <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-      </div>
+    <div class="container">
+        <div class="card">
+          <div class="card-header">
+            Data Diri
+          </div>
+          <div class="card-body">
+              <form method="post" action="index.php">
+               <div class="form-group">
+                    <input type="text" name="name" class="form-control" placeholder="Name"/>
+               <div>
+                   
+               <div class="form-group">
+                    <input type="text" name="email" class="form-control" placeholder="E-Mail"/>
+               <div>
+                   
+               
+               <div class="form-group">
+                    <input type="text" name="job" class="form-control" placeholder="Jobs"/>
+               <div>
+                   
+               
+               <div class="form-group">
+                    <input type="date" name="date" class="form-control" placeholder="Date"/>
+               <div>
+               <input type="submit" name="submit" class="btn btn-primary"/>
+              </form>
+          </div>
+        </div>
     </div>
     
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
